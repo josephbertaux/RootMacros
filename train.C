ee7e008b97db44713e9d4cb7b6eafc9b8a6ccce3
josephbertaux <jbertau@purdue.edu>
2022-03-02 12:54:00 -0500

    Sample Text

---
+++ train.C
@@ -0,0 +1,66 @@
+#include "TMVA/DataLoader.h"
+#include "TMVA/Factory.h"
+
+#include "TFile.h"
+#include "TString.h"
+#include "TTree.h"
+
+
+void train()
+{
+	//--Option specification--//
+	const char* training_branches_filename = "training_branches.list";
+	const char* sgnl_tree_filename = "sgnl.root";
+	const char* bkgd_tree_filename = "bkgd.root";
+
+	const char* weights_filename = "weights";
+	const char* data_loader_filename = "dataset";
+
+	TString factory_options = "V:!Silent:Color:Transformations=I:DrawProgressBar:AnalysisType=Classification";
+	TString dataset_options = "SplitMode=Random";
+	TString method_options = "";
+
+	TCut sgnl_cut = "";
+	TCut bkgd_cut = "";
+
+	//--Training starts here--//
+	TMVA::Tools::Instance();
+	
+	TFile* out_file = TFile::Open("training_out.root", "RECREATE");
+	TMVA::Factory *factory = new TMVA::Factory(weights_filename, out_file,factory_options);
+	TMVA::DataLoader *dl = new TMVA::DataLoader(data_loader_filename);
+
+	//Add training variables to the DataLoader which are the branches of interest
+	ifstream training_branches(training_branches_filename);
+	string branch_name;
+
+	while(true)
+	{
+		training_branches >> branch_name;
+		if(training_branches.eof())
+		{
+			break;
+		}
+
+		dl->AddVariable(branch_name.c_str(), 'F');
+	}
+	
+	TFile* sgnl_file(TFile::Open(sgnl_tree_filename));
+	TTree* sgnl_tree = (TTree*)sgnl_file->Get("Signal"); //The name of the signal tree is "Signal"
+	dl->AddSignalTree(sgnl_tree, 1.0); //Nothing special with the weights
+
+	TFile* bkgd_file(TFile::Open(bkgd_tree_filename));
+	TTree* bkgd_tree = (TTree*)bkgd_file->Get("Background"); //The name of the background tree is "Background"
+	dl->AddBackgroundTree(bkgd_tree, 1.0); //Nothing special with the weights
+
+	dl->PrepareTrainingAndTestTree(sgnl_cut, bkgd_cut, dataset_options);
+	factory->BookMethod(dl, TMVA::Types::kBDT, "BDT", method_options);
+
+	factory->TrainAllMethods();
+	factory->TestAllMethods();
+	factory->EvaluateAllMethods();
+
+	if(!gROOT->IsBatch()){TMVA::TMVAGui(out_file);}
+	
+	out_file->Close();
+}
